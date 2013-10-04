9ad85a902c86f6caa606846388bf4564f77a5eb1
unknown <pgonay>
2013-10-04 15:40:21 +0700

    Pertemuan 4 dan 5

---
+++ pertemuan4/Praktikum/Praktikum Soal 9/Praktikum.java
@@ -0,0 +1,14 @@
+public class Praktikum{
+	public static void main (String[] args){
+		int a=4,b=6;
+		int num=a|b;
+		if (num==4)
+		System.out.println ("value is 4");
+		
+		System.out.println ("value is 5");
+		
+		System.out.println ("value is 6");
+	
+		System.out.println ("value is 7");
+	}
+}
